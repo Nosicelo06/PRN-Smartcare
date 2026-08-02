1ca5695e61ffef3d7781d3672e8bf7f09f95dd61
Nosicelo06 <amahlenosicelo2@gmail.com>
2026-08-02 11:47:01 +0200

    implementuser authentication with login and registartion

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Patient(models.Model):
+    Id_number = models.CharField(max_length=13, unique=True)
+    prn_number = models.CharField(max_length=15, unique=True)
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100, blank=True)
+    surname = models.CharField(max_length=100)
+    gender = models.CharField(max_length=10)
+    date_of_birth = models.DateField()
+    phone_number = models.CharField(max_length=15)
+    Address = models.TextField()
+    def _str_(self):
+        return f"{self.first_name}{self.surname}".strip()
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,64 @@
-from django.shortcuts import render
+print("THIS ISMY VIEWS FILE")
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.contrib import messages
 
-# Create your views here.
+
 def login_view(request):
-    return render(request,'accounts/login.html')
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
 
-# Create your views here
+        user = authenticate(
+            request,
+            username=email,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+            return redirect("dashboard")
+        else:
+            messages.error(request, "Invalid email or password")
+
+    return render(request, "accounts/login.html")
+
+
 def register_view(request):
-    return render(request, 'accounts/register.html')
+    if request.method == "POST":
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        email = request.POST.get("email")
+        password1 = request.POST.get("password1")
+        confirmpassword = request.POST.get("confirmpassword")
+
+        #check if passwords match
+        if password1 != confirmpassword :
+            messages.error(request, "Passwords do not match.")
+            return redirect ("register")
+
+        # check if email already exists
+        if user.objects.filter(email=email).exists():
+            messages.error(request, "An account with this email already exists.")
+            return redirect("register")
+            
+        # Create a user
+        User.objects.create_user(
+            username=email,
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+            password=password1,
+        )
+
+        StaffProfile.objects.create(
+            user=user,
+            surname=surname,
+            cell_number=cell_number,
+            role=role,
+        )
+
+        return redirect("login")
+
+    return render(request, "accounts/register.html")
