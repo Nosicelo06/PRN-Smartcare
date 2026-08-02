883d394ad5061013381ae9070b67e36138bb945e
Nosicelo06 <amahlenosicelo2@gmail.com>
2026-08-02 11:54:45 +0200

    Implement user authentication with login and registration

--- accounts/models.py
+++ accounts/models.py
@@ -1,17 +1,18 @@
+from django.contrib.auth.models import user
 from django.db import models
 
 # Create your models here.
+class StaffProfile(models.Model):
+    user = models.oneTooneField(User, on_delete=models.CASCADE)
 
-class Patient(models.Model):
-    Id_number = models.CharField(max_length=13, unique=True)
-    prn_number = models.CharField(max_length=15, unique=True)
-    first_name = models.CharField(max_length=100)
-    middle_name = models.CharField(max_length=100, blank=True)
-    surname = models.CharField(max_length=100)
-    gender = models.CharField(max_length=10)
-    date_of_birth = models.DateField()
-    phone_number = models.CharField(max_length=15)
-    Address = models.TextField()
-    def _str_(self):
-        return f"{self.first_name}{self.surname}".strip()
+    surname = models.Charfield(max_length=100)
+    cell_number = models.Charfield(max_length=15)
 
+    ROLE_CHOICES = [
+        ("Admin", "Administrator"),
+        ("Receptionist", "Receptionist"),
+        ("Doctor", "Doctor"),
+        ("Nurse", "Nurse"),
+        ("Pharmacist","Phamacist"),
+    ]
+    role = models.Charfield(max_length=20, choices=ROLE_CHOICES)
